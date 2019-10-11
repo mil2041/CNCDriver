6c34c0648e82e7db622e8854f00bd091ec261bf5
Eric Liu <mil2041@med.cornell.edu>
2019-10-11 02:36:28 -0400

    rebuild data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' @title reduced funseq2 CDS output for LGG
+#' @description This data contains the topology of a 
+#' circuit with ten genes A1...A5 and B1...B5. 
+#' @format A data frame with 18 rows and 3 variables
+"reducedFunseqOutputCDS"
+
+#' @title reduced funseq2 NCDS output for LGG
+#' @description This data contains the topology of a 
+#' circuit with ten genes A1...A5 and B1...B5. 
+#' @format A data frame with 18 rows and 3 variables
+"reducedFunseqOutputNCDS"
